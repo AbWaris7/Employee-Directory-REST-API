82789c284458c19d96bc9f65845a7395b1807a2a
abdulwariskhan <hellokhan.ch@gmail.com>
2025-04-26 20:14:54 +0200

    Initial commit

---
+++ src/test/java/com/khan/code/Employee/Directory/REST/API/EmployeeDirectoryRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khan.code.Employee.Directory.REST.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDirectoryRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
